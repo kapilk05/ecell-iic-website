ac8dbaf7abda3b63af3c6808ebe24402424ef87b
SaakshiShah25 <shahsaakshi25@gmail.com>
2020-06-20 21:01:43 +0530

    Adding menubar

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 function onClickMenu(){
     document.getElementById("hamburger").classList.toggle("change");
-    document.getElementById('row1').classList.toggle("change");
-    document.getElementById('row2').classList.toggle("change");
+    document.getElementById('row1').classList.toggle("change-row");
+    document.getElementById('row2').classList.toggle("change-row");
     document.getElementById('menu-bg').classList.toggle("change-bg");
 }
 
 $("#readmore").hover(function() {
-    document.getElementById('heading').classList.toggle("hvr-underline-from-left");
     document.getElementById('readmore').classList.toggle("hvr-icon-forward");
-    
+});
+
+ $("#readmore").hover(function(){
+    document.getElementById('heading').classList.toggle("hvr-underline-from-left");
  });
 
- 
+ $("#arrow").hover(function(){
+     $('#iconid').style.display="inline";
+    document.getElementById('arrow').classList.toggle("hvr-icon-forward");
+ });
