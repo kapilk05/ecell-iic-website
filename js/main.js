7418aacd4502c849d1028fe7c6b31f4526db0b96
SaakshiShah25 <shahsaakshi25@gmail.com>
2020-07-01 12:20:50 +0530

    Adding gallery

--- js/main.js
+++ js/main.js
@@ -17,3 +17,96 @@ $("#readmore").hover(function() {
      $('#iconid').style.display="inline";
     document.getElementById('arrow').classList.toggle("hvr-icon-forward");
  });
+
+
+/* Events Gallery */
+const $left = $(".left");
+const $gl = $(".gallery");
+const $gl2 = $(".gallery2");
+const $photosCounterFirstSpan = $(".photos-counter span:nth-child(1)"); 
+
+$gl.slick({
+    rows: 0,
+    slidesToShow: 2,
+    arrows: false,
+    draggable: false,
+    useTransform: false,
+    mobileFirst: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 1023,
+        settings: {
+          slidesToShow: 1,
+          vertical: true
+        }
+      }
+    ]
+  });
+   
+  $gl2.slick({
+    rows: 0,
+    useTransform: false,
+    prevArrow: ".arrow-left",
+    nextArrow: ".arrow-right",
+    fade: true,
+    asNavFor: $gl
+  });
+
+  $(window).on("load", () => {
+    handleCarouselsHeight();
+    setTimeout(() => {
+      $(".loading").fadeOut();
+      $("body").addClass("over-visible");
+    }, 300);
+  });
+
+  function handleCarouselsHeight() {
+    if (window.matchMedia("(min-width: 1024px)").matches) {
+      const gl2H = $(".gallery2)").height();
+      $left.css("height", gl2H);
+    } else {
+      $left.css("height", "auto");
+    }
+  }
+
+  $(window).on(
+    "resize",
+    _.debounce(() => {
+      handleCarouselsHeight();
+    }, 200)
+  );
+
+/*you have to bind init event before slick's initialization (see demo) */
+gl2.on("init", (event, slick) => {
+    $photosCounterFirstSpan.text(`${slick.currentSlide + 1}/`);
+    $(".photos-counter span:nth-child(2)").text(slick.slideCount);
+  });
+   
+  $gl2.on("afterChange", (event, slick, currentSlide) => {
+    $photosCounterFirstSpan.text(`${slick.currentSlide + 1}/`);
+  });
+
+$(".gallery .item").on("click", function() {
+    const index = $(this).attr("data-slick-index");
+    $gl2.slick("slickGoTo", index);
+  });
+
+
+/*Map section*/
+mapboxgl.accessToken = '<your access token here>';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v11',
+center: [12.550343, 55.665957],
+zoom: 8
+});
+ 
+var marker = new mapboxgl.Marker()
+.setLngLat([12.550343, 55.665957])
+.addTo(map);
