0f05f51ae68ffe7b84b5386f1785f3f79154bdd9
SakshiUppoor <sakshiuppoor@gmail.com>
2020-07-06 14:27:39 +0530

    Make navbar responsive

--- js/nav.js
+++ js/nav.js
@@ -1,3 +1,47 @@
 $(window).scroll(function () {
   $("nav").toggleClass("scrolled", $(this).scrollTop() > 50);
+  $("nav").toggleClass("navbar-dark", $(this).scrollTop() <= 50);
+  $("nav").toggleClass("navbar-light", $(this).scrollTop() > 50);
 });
+
+// window.change(function () {
+//   if (!$(".navbar-toggle").hasClass("collapsed")) {
+//     console.log("expand");
+//     $("video").addClass("extend-vid");
+//     $(".fullscreen-video-wrap").addClass("extend-vid");
+//     $(".header-overlay").addClass("extend-vid");
+//   } else {
+//     console.log("collapse");
+//     $("video").toggleClass("extend-vid");
+//     $(".fullscreen-video-wrap").toggleClass("extend-vid");
+//     $(".header-overlay").toggleClass("extend-vid");
+//   }
+// });
+
+function extend() {
+  $("video").toggleClass("extend-vid");
+  $(".fullscreen-video-wrap").toggleClass("extend-vid");
+  $(".header-overlay").toggleClass("extend-vid");
+}
+
+function clickNavLink() {
+  console.log(!$(".navbar-toggle").hasClass("collapsed"));
+  if (!$(".navbar-toggle").hasClass("collapsed")) {
+    $(".navbar-toggler").toggleClass("collapsed");
+    $(".navbar-collapse").toggleClass("show");
+    extend();
+  }
+}
+
+function clickToggler() {
+  if ($(".navbar-collapse").hasClass("show")) {
+    $(".navbar-toggler").click();
+  }
+}
+
+$(".navbar-toggler").click(extend);
+
+$(".nav-link").click(clickNavLink);
+
+$("main").click(clickToggler);
+$(".v-header").click(clickToggler);
