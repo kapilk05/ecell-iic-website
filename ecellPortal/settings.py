5cfd10ce2ac0edd1e458a2d2debcdcb35799103a
Saakshi <shahsaakshi25@gmail.com>
2020-07-24 19:40:25 +0530

    Fetching api data successful

--- ecellPortal/settings.py
+++ ecellPortal/settings.py
@@ -38,10 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'ecellApp'
+    'ecellApp',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', # new
+    'django.middleware.common.CommonMiddleware', # new
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,6 +53,11 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ORIGIN_WHITELIST = (
+    'http://127.0.0.1:3000',
+    
+)
 
 ROOT_URLCONF = 'ecellPortal.urls'
 
--- ecellfrontend/src/pages/startup.js
+++ ecellfrontend/src/pages/startup.js
@@ -7,42 +7,35 @@ export class Startup extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      Startups: [],
+      startups: [],
     };
   }
-  getStartupsData() {
-    axios
-      .get(`http://127.0.0.1:8000/api/startup/`)
-      .then((res) => {
-        const data = res.data;
-        console.log(data);
-
-        const startup = data.map((u) => (
-          <div class="row" id="card_display">
-            <div class="col-md-3" id="column_startup">
-              <img class="startup_holder" src={u.logo} />
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/api/startup/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((startups) => {
+        this.setState({ startups });
+      });
+  }
+  render() {
+    return (
+      <div className="container section-holder" id="startup">
+        <h1 className="">Startups</h1>
+        <div className="row" id="card_display">
+          {this.state.startups.map((u) => (
+            <div className="col-md-3" id="column_startup">
+              <img className="startup_holder" src={u.logo} />
               <p>{u.intro}</p>
               <Link to={u.link} class="peach-text startup_more" />
               Read More
             </div>
-          </div>
-        ));
-        this.setState({
-          startup,
-        });
-      })
-      .catch((e) => {
-        console.log(e);
-      });
-  }
-  componentDidMount() {
-    this.getStartupsData();
-  }
-  render() {
-    return (
-      <div class="container section-holder" id="startup">
-        <h1 class="">Startups</h1>
-        {this.state.startup}
+          ))}
+        </div>
       </div>
     );
   }
