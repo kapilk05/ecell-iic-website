cb65e11f07ad15ba8feeb8a4525ed9569f0967d4
Saakshi Shah <shahsaakshi25@gmail.com>
2020-08-27 16:19:54 +0530

    Merge

--- ecellfrontend/src/components/getstarted.js
+++ ecellfrontend/src/components/getstarted.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { findDOMNode } from "react-dom";
 import "../style.css";
 import { Link } from "react-router-dom";
 import { Icon } from "react-icons-kit";
@@ -6,20 +7,26 @@ import { mortarBoard } from "react-icons-kit/typicons/mortarBoard";
 import { scissorsOutline } from "react-icons-kit/typicons/scissorsOutline";
 import { chartLineOutline } from "react-icons-kit/typicons/chartLineOutline";
 import { planeOutline } from "react-icons-kit/typicons/planeOutline";
+import $ from "jquery";
 
 export class GetStarted extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      on: false,
-    };
+    this.showForm = this.showForm.bind(this);
   }
 
-  showForm() {
-    this.setState({
-      on: !this.state.on,
-    });
-  }
+  showForm = () => {
+    console.log("Hi!");
+    const el = findDOMNode(this.refs.toggle);
+    $(el).style.display = "block";
+    $("html,body").animate(
+      {
+        scrollTop: this.myInput.offset().top,
+      },
+      100
+    );
+  };
+
   render() {
     return (
       <div id="features">
@@ -87,10 +94,7 @@ export class GetStarted extends Component {
           <div className="main container section-holder">
             <h4>Have a business idea?</h4>
             <h1 className="cta">Launch your startup!</h1>
-            <button
-              onclick={() => this.showForm()}
-              className="btn hvr-icon-forward"
-            >
+            <button onclick={this.showForm} className="btn hvr-icon-forward">
               Get Started
               <i className="zmdi zmdi-arrow-right hvr-icon"></i>
             </button>
@@ -104,127 +108,126 @@ export class GetStarted extends Component {
               </div>
             </div>
 
-            <div className="container" id="idea_submission">
+            <div
+              className="container"
+              ref="toggle"
+              style={{
+                display: "none",
+              }}
+            >
               <div className="row">
                 <div className="col-md-2"></div>
 
                 <div className="col-md-8 idea_form">
-                  {this.state.on ? (
-                    <div className="col-md-8 idea_form">
-                      <h1>Submit your Idea</h1>
-                      <label for="name">Full Name</label>
-                      <input name="name" type="text" id="name" />
-                      <br />
-                      <div className="row">
-                        <div className="col-md-6">
-                          <label for="email">Email</label>
-                          <input
-                            name="email"
-                            type="email"
-                            id="email"
-                            required
-                          />
-                          <br />
-                        </div>
-                        <div className="col-md-6">
-                          <label for="contact">Contact Number</label>
-                          <input name="contact" type="tel" id="contact" />
-                          <br />
-                        </div>
+                  <div className="col-md-8 idea_form">
+                    <h1>Submit your Idea</h1>
+                    <label for="name">Full Name</label>
+                    <input name="name" type="text" id="name" />
+                    <br />
+                    <div className="row">
+                      <div className="col-md-6">
+                        <label for="email">Email</label>
+                        <input name="email" type="email" id="email" required />
+                        <br />
                       </div>
-
-                      <div className="row">
-                        <div className="col-md-6 radioStudent">
-                          Are you a student?
-                          <br />
-                          <div className="radioButton">
-                            <input
-                              type="radio"
-                              id="male"
-                              name="student"
-                              value="Yes"
-                              onclick="showStudent();"
-                            />
-                            <label for="Yes">Yes</label>
-                            <br />
-                            <input
-                              type="radio"
-                              id="No"
-                              name="student"
-                              value="No"
-                            />
-                            <label for="No">No</label>
-                            <br />
-                          </div>
-                        </div>
-                        <div className="col-md-6 radioBusiness">
-                          Type of business
-                          <select id="business" name="business">
-                            <option value="Sole Proprietorship" selected>
-                              Sole Proprietorship
-                            </option>
-                            <br />
-                            <option value="Partnership">Partnership</option>
-                            <br />
-                          </select>
-                          <br />
-                        </div>
+                      <div className="col-md-6">
+                        <label for="contact">Contact Number</label>
+                        <input name="contact" type="tel" id="contact" />
+                        <br />
                       </div>
-                      <div className="row" id="studentInfo">
-                        <div className="col-md-6">
-                          <select id="stream" name="stream">
-                            <option value="CSE" selected>
-                              CSE
-                            </option>
-                            <br />
-                            <option value="Computer">Computer</option>
-                            <br />
-                            <option value="IT">IT </option>
-                            <br />
-                            <option value="EXTC">EXTC</option>
-                            <br />
-                            <option value="Electronics">Electronics </option>
-                            <br />
-                            <option value="Chemical">Chemical</option>
-                            <br />
-                            <option value="Mechanical">Mechanical</option>
-                            <br />{" "}
-                          </select>
-                          <br />
-                        </div>
-                        <div className="col-md-6">
-                          <select id="year" name="year">
-                            <option value="FE" selected>
-                              FE
-                            </option>
-                            <br />
-                            <option value="SE">SE</option>
-                            <br />
-                            <option value="TE">TE</option>
-                            <br />
-                            <option value="BE">BE </option>
-                            <br />{" "}
-                          </select>
-                          <br />
-                        </div>
-                      </div>
-                      <textarea
-                        rows="4"
-                        cols="72"
-                        name="idea"
-                        form="idea"
-                        placeholder="Tell us about your idea!"
-                      ></textarea>
-                      <button
-                        className="btn"
-                        type="submit"
-                        id="submit_idea"
-                        style={{ "margin-top": "10px" }}
-                      >
-                        Submit
-                      </button>
                     </div>
-                  ) : null}
+
+                    <div className="row">
+                      <div className="col-md-6 radioStudent">
+                        Are you a student?
+                        <br />
+                        <div className="radioButton">
+                          <input
+                            type="radio"
+                            id="male"
+                            name="student"
+                            value="Yes"
+                            onclick="showStudent();"
+                          />
+                          <label for="Yes">Yes</label>
+                          <br />
+                          <input
+                            type="radio"
+                            id="No"
+                            name="student"
+                            value="No"
+                          />
+                          <label for="No">No</label>
+                          <br />
+                        </div>
+                      </div>
+                      <div className="col-md-6 radioBusiness">
+                        Type of business
+                        <select id="business" name="business">
+                          <option value="Sole Proprietorship" selected>
+                            Sole Proprietorship
+                          </option>
+                          <br />
+                          <option value="Partnership">Partnership</option>
+                          <br />
+                        </select>
+                        <br />
+                      </div>
+                    </div>
+                    <div className="row" id="studentInfo">
+                      <div className="col-md-6">
+                        <select id="stream" name="stream">
+                          <option value="CSE" selected>
+                            CSE
+                          </option>
+                          <br />
+                          <option value="Computer">Computer</option>
+                          <br />
+                          <option value="IT">IT </option>
+                          <br />
+                          <option value="EXTC">EXTC</option>
+                          <br />
+                          <option value="Electronics">Electronics </option>
+                          <br />
+                          <option value="Chemical">Chemical</option>
+                          <br />
+                          <option value="Mechanical">Mechanical</option>
+                          <br />{" "}
+                        </select>
+                        <br />
+                      </div>
+                      <div className="col-md-6">
+                        <select id="year" name="year">
+                          <option value="FE" selected>
+                            FE
+                          </option>
+                          <br />
+                          <option value="SE">SE</option>
+                          <br />
+                          <option value="TE">TE</option>
+                          <br />
+                          <option value="BE">BE </option>
+                          <br />{" "}
+                        </select>
+                        <br />
+                      </div>
+                    </div>
+                    <textarea
+                      rows="4"
+                      cols="72"
+                      name="idea"
+                      form="idea"
+                      placeholder="Tell us about your idea!"
+                    ></textarea>
+                    <button
+                      className="btn"
+                      type="submit"
+                      id="submit_idea"
+                      style={{ "margin-top": "10px" }}
+                    >
+                      Submit
+                    </button>
+                  </div>
                 </div>
                 <div className="col-md-2"></div>
               </div>
