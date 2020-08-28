3a81c74da2856934d94249f8573a9c5e0bc02e5a
Saakshi Shah <shahsaakshi25@gmail.com>
2020-08-28 17:49:21 +0530

    Merge

--- originalFiles/js/Gsheet.js
+++ originalFiles/js/Gsheet.js
@@ -1,14 +1,28 @@
-var $form = $('form#test-form'),
-    url = 'https://script.google.com/macros/s/AKfycbwFxy4hsIK-JeblK2dDbI7G4RJf2LPNhO3U1Ss8q24KsvVaeb16/exec'
+var $form = $("form#startup_idea"),
+  url =
+    "https://script.google.com/macros/s/AKfycbwFxy4hsIK-JeblK2dDbI7G4RJf2LPNhO3U1Ss8q24KsvVaeb16/exec";
 
-$('#submit-form').on('click', function(e) {
+$("#submit_idea").on("click", function (e) {
   e.preventDefault();
   var jqxhr = $.ajax({
     url: url,
     method: "GET",
     dataType: "json",
-    data: $form.serializeObject()
-  }).success(
+    data: $form.serializeObject(),
+  })
+    .success
     // Do what you want with this Uper all yours!
-  );
-})
+    ();
+});
+
+// const scriptURL =
+//   "https://script.google.com/macros/s/AKfycbwFxy4hsIK-JeblK2dDbI7G4RJf2LPNhO3U1Ss8q24KsvVaeb16/exec";
+// const form = document.forms["e-cell"];
+
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   fetch(scriptURL, { method: "POST", body: new FormData(form) })
+//     .then(alert("Details updated"))
+//     .then(location.reload())
+//     .catch((error) => console.error("Error!", error.message));
+// });
