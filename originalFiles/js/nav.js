b6812d9bfbc7203b8725cdb6f95acd3a15685aa3
SakshiUppoor <sakshiuppoor@gmail.com>
2020-08-28 16:25:19 +0530

    Fix nav and partially clean CDN
    Toast is broken

--- originalFiles/js/nav.js
+++ originalFiles/js/nav.js
@@ -1,69 +1,45 @@
-// window.change(function () {
-//   if (!$(".navbar-toggle").hasClass("collapsed")) {
-//     console.log("expand");
-//     $("video").addClass("extend-vid");
-//     $(".fullscreen-video-wrap").addClass("extend-vid");
-//     $(".header-overlay").addClass("extend-vid");
-//   } else {
-//     console.log("collapse");
-//     $("video").toggleClass("extend-vid");
-//     $(".fullscreen-video-wrap").toggleClass("extend-vid");
-//     $(".header-overlay").toggleClass("extend-vid");
-//   }
-// });
+function extendVid() {
+  $("video").addClass("extend-vid");
+  $(".fullscreen-video-wrap").addClass("extend-vid");
+  $(".header-overlay").addClass("extend-vid");
+}
 
-function extend() {
-  if (window.innerWidth <= 992) {
-    if (!$(".navbar-collapse").hasClass("show")) {
-      $("video").addClass("extend-vid");
-      $(".fullscreen-video-wrap").addClass("extend-vid");
-      $(".header-overlay").addClass("extend-vid");
+function unextendVid() {
+  $("video").removeClass("extend-vid");
+  $(".fullscreen-video-wrap").removeClass("extend-vid");
+  $(".header-overlay").removeClass("extend-vid");
+}
+
+$(".navbar-toggler").click(() => {
+  if (!$(".navbar-collapse").hasClass("show")) {
+    extendVid();
+  } else {
+    unextendVid();
+  }
+});
+
+var width = $(window).width();
+$(window).on("resize", function () {
+  if ($(this).width() !== width) {
+    width = $(this).width();
+    if (width > 1200) {
+      if ($(".navbar-collapse").hasClass("show")) {
+        unextendVid();
+      }
     } else {
-      $("video").toggleClass("extend-vid");
-      $(".fullscreen-video-wrap").toggleClass("extend-vid");
-      $(".header-overlay").toggleClass("extend-vid");
+      if ($(".navbar-collapse").hasClass("show")) {
+        extendVid();
+      }
     }
   }
-}
-
-function clickNavLink() {
-  if (!$(".navbar-toggle").hasClass("collapsed")) {
-    $(".navbar-toggler").toggleClass("collapsed");
-    $(".navbar-collapse").toggleClass("show");
-    extend();
-  }
-}
-
-function clickToggler() {
-  if ($(".navbar-collapse").hasClass("show")) {
-    $(".navbar-toggler").toggleClass("collapsed");
-  }
-  $(".navbar-collapse").toggleClass("show");
-  console.log($(".navbar-collapse").hasClass("show"));
-}
-
-$(".navbar-toggler").click(extend);
-// $(".navbar-toggler").click(clickToggler);
-
-$(".nav-link").click(clickNavLink);
-
-// $("main").click(clickToggler);
-// $(".v-header").click(clickToggler);
-
-$(window).scroll(function () {
-  if ($(this).scrollTop() >= 50) {
-    // If page is scrolled more than 50px
-    $("#takeup").fadeIn(200); // Fade in the arrow
-  } else {
-    $("#takeup").fadeOut(200); // Else fade out the arrow
-  }
 });
-$("#takeup").click(function () {
-  // When arrow is clicked
-  $("body,html").animate(
-    {
-      scrollTop: 0, // Scroll to top of body
-    },
-    500
-  );
+
+$(".nav-link").click(() => {
+  $(".navbar-toggler").click();
 });
+
+$("body > *")
+  .not("body > nav")
+  .click(() => {
+    $(".navbar-toggler").click();
+  });
