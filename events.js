3c8aa154204df8aed553b5e015321fb69eef336d
Krish <krishshah235@gmail.com>
2021-11-12 17:04:42 +0530

    Added core team and cerificates section

--- events.js
+++ events.js
@@ -1,71 +1,85 @@
 events_list = [
     {
+        "id": "redbull-event",
+        "title": "Redbull Event",
+        "date": "2018-19",
+        "conductedBy": "Red Bull",
+        "desc": `
+        Hacking for a Brighter Campus
+        <br />
+        Red Bull Basement connects the next generation of innovators to turn forward thinking solutions into reality.
+        `,
+        "coverImage": "event_img.png",
+        "shortDesc": "Hacking for a Brighter Campus",
+        "tag": "2018-19"
+    },
+    {
         "id": "genesis",
         "title": "Genesis",
         "date": "29th September 2019",
-        "conductedBy":"Viraj Ghelani from Filtercopy",
+        "conductedBy": "Viraj Ghelani from Filtercopy",
         "desc": `
         DJSCE E-CELL had organized an interactive session with a well known digital content creator <b>Viraj Ghelani</b> who is also from an engineering background. The purpose of the session was to inspire students from the life journey of one of the most popular social media faces. It helped students understand the importance of following their dreams and passion. Viraj also briefed the students regarding his struggles and determination that led him in becoming a content creator for digital entertainment at Pocket Aces. The session was filled with a lot of humor and interaction that led to a spirit of enthusiasm inside the seminar hall.
         `,
-        "coverImage":"event_img.jpg",
-        "shortDesc":"An interactive session with a well known digital content creator <b>Viraj Ghelani</b> who is also from an engineering background.",
-        "tag":"2019-20"
+        "coverImage": "event_img.jpg",
+        "shortDesc": "An interactive session with a well known digital content creator <b>Viraj Ghelani</b> who is also from an engineering background.",
+        "tag": "2019-20"
     },
     {
         "id": "python-workshop",
         "title": "Python Workshop",
         "date": "14th-15th December, 2019",
-        "conductedBy":"Jinesh Doshi and Nimesh Bumb from 'Masterbridge'",
+        "conductedBy": "Jinesh Doshi and Nimesh Bumb from 'Masterbridge'",
         "desc": `
         This event was organized by the Ecell Committee of our college to give the students the opportunity to be inspired by the two hard-working entrepreneurs- <b>Jinesh Doshi and Nimesh Bumb</b>, from <b>‘Masterbridge’</b>. This was a company started by them with the vision of helping and preparing Indian students, face the challenges and build a career in foreign countries. Python has climbed its way to the top skills garnering some of the highest salaries and sought after jobs in the industry. This made the free workshop provided, not only very useful but also an exciting and fun experience to spend the weekend in the company of their fellow peers and friends. It made the vacation more productive and really motivated the students to keep learning and being curious.
         `,
-        "coverImage":"6a7fb3b4-93f0-4c88-94e4-47ec3aaf9fd6.jpg",
-        "shortDesc":"An event where students learned and interacted with two hard-working entrepreneurs from Masterbridge.",
-        "tag":"2019-20"
+        "coverImage": "6a7fb3b4-93f0-4c88-94e4-47ec3aaf9fd6.jpg",
+        "shortDesc": "An event where students learned and interacted with two hard-working entrepreneurs from Masterbridge.",
+        "tag": "2019-20"
     },
     {
         "id": "rush-hour-2020",
         "title": "Rush Hour 2020",
         "date": "13th March 2020",
-        "conductedBy":"DJSCE E-CELL committee",
+        "conductedBy": "DJSCE E-CELL committee",
         "desc": `
         DJSCE E-CELL conducted its flagship event <b>RUSH HOUR 2020</b> which was the most awaited sales competition of the year. We had the most amazing experience with everything going exactly as perceived. From MADvertising to choose the team leaders, to trying and get the highest bid. The event was filled with fun, games, and a lot of entertainment. The students had a wonderful experience and indeed it was a great opportunity for them to nurture their marketing skills. The event gave students the chance to become the brand ambassador of the stall and increase the sales on the basis of a rulebook provided. The stalls that took part in the competition included <b>PINITUP</b> badges, <b>WAI WAI noodles</b>, <b>YUMMY MOMOS</b>, <b>BOMBAY DESSERT COMPANY</b> and <b>DR.BUBBLES.</b>
         `,
-        "coverImage":"WhatsApp Image 2020-03-13 at 10.31.34 PM.jpeg",
-        "shortDesc":"E-CELL's flagship event which was the most awaited sales competition of the year.",
-        "tag":"2019-20"
+        "coverImage": "WhatsApp Image 2020-03-13 at 10.31.34 PM.jpeg",
+        "shortDesc": "E-CELL's flagship event which was the most awaited sales competition of the year.",
+        "tag": "2019-20"
     },
     {
         "id": "faculty-meetup",
         "title": "Faculty Meetup",
         "date": "25th February 2020",
-        "conductedBy":"DJSCE E-CELL faculty",
+        "conductedBy": "DJSCE E-CELL faculty",
         "desc": `
         DJSCE E-CELL had organized a faculty meet-up to convert SIH ideas and project into a full fledged startup. The aim of the meet up was to educate students  regarding the <b>National Innovation Contest 2020</b> organised by IIC , in collaboration with the Government of India's Innovation cell and MHRD. It is an initiative  where top startups get funding, incubation support and international visits. The faculty had highlighted various market scenarios ,  competition  factors and  other key investment opportunities for our budding entrepreneurs.
         `,
-        "coverImage":"3d962f30-fc39-4d8e-b049-1c7e42539aed.jpg",
-        "shortDesc":"A faculty meet-up to convert SIH ideas and project into a full fledged startup.",
-        "tag":"2019-20"
+        "coverImage": "3d962f30-fc39-4d8e-b049-1c7e42539aed.jpg",
+        "shortDesc": "A faculty meet-up to convert SIH ideas and project into a full fledged startup.",
+        "tag": "2019-20"
     },
     {
         "id": "dj-talks",
         "title": "DJ Talks",
         "date": "10th January 2020",
-        "conductedBy":"Gaurav Jain Founding member of XBillion Skillz Lab, and Shronit Ladhani CEO of CareerNinja",
+        "conductedBy": "Gaurav Jain Founding member of XBillion Skillz Lab, and Shronit Ladhani CEO of CareerNinja",
         "desc": `
         <b>Gaurav Jain</b>, Founding member of <b>XBillion Skillz Lab</b> is an ambitious leader with strong intent to solve the problem of unemployment in India. Being a former Railway Ministry official he was able to open our mind and make us understand how the system works. He being an intellectual gave us meaningful insight into his ecosystem of impact enterprises with the collective aim of to leverage technology, behavior sciences and practical modules to develop the ability to apply innate natural intelligence for situations, opportunities and challenges at the modern workplace.By making us understand with simplicity about artificial intelligence, and why his mission is important to ensure we do not get completely replaced by automation, he was able to make us view things in a completely different perspective other than our standard beliefs that our CV, our degree and CGPA determine the placements we get. He answered all the students' curious questions. The students seemed to have enjoyed learning about his inspiring journey, which to be honest most of us dream of.
         </br></br>
         On the other hand we had <b>Shronit Ladhani</b>, CEO of <b>CareerNinja</b>. So hard-working, yet at the age of 27 has visited 38 countries and plans on reaching his goal of a 50 soon maybe by the end of the year. He was able to win all our trust because he was a young role model no different from us in terms of normal graduation. We immediately started believing his ideologies and being inspired by him. We gained a lot of useful tips on how we can grow as an employee and gain maximum exposure. Everyone had smiles on their face at the end of this long, informative, but also exciting and delightful session of DJ Talks.
         `,
-        "coverImage":"img3.JPG",
-        "shortDesc":"A talk by Gaurav Jain and Shronit Ladhani.",
-        "tag":"2019-20"
+        "coverImage": "img3.JPG",
+        "shortDesc": "A talk by Gaurav Jain and Shronit Ladhani.",
+        "tag": "2019-20"
     },
     {
         "id": "business-canvas-workshop",
         "title": "Business Canvas Workshop",
         "date": "28th and 29th January 2020",
-        "conductedBy":"Dr Indira Singh, PHD in Entrepreneurship",
+        "conductedBy": "Dr Indira Singh, PHD in Entrepreneurship",
         "desc": `
         In this workshop, students had the opportunity to create a business canvas model of their own Idea. We at E-Cell will guided them with the help of Experienced professionals- <b>Prof. Pavan Kumar Borra</b>, <b>Dr. Sunil Karamchandani</b> from our very own college, and <b>Dr Indira Singh</b>, PHD in Entrepreneurship.In this workshop, students had the opportunity to create a business canvas model of their own Idea. We at E-Cell will guided them with the help of Experienced professionals-Prof. Pavan Kumar Borra, from our very own college, and Dr Indira Singh, PHD in Entrepreneurship.Even if the students did not have any ideas of their own, they could still learn and understand how to fulfill their dreams by our team's brilliant coordination to help bring the students the best business canvas model exposure. We had a list of thought-provoking ideas that they could choose from to learn through examples.Our prime focus was to give the students a remarkable opportunity to be registered at Atal Incubation Centre (AIC). Our speaker, Dr Indira Singh is associated with them and gave such wonderful insights into the unknown world of business models to us engineers. She discussed, with great interest, some of the most beautiful theories of business strategy- Blue Ocean and Red Ocean strategy. She taught us, from her wide intellectual and teaching experience, to identify and communicate the nine key elements of a business model:
         <br><br>
@@ -74,14 +88,14 @@ events_list = [
         The case studies of Jio, Uber, Chumbak, AirBnB and Zomato, were discussed with great interest. The students seemed to have gained a lot of benefit from our <b>co-chairperson Vatsal Gupta's </b>introduction on entrepreneurship, even more from Pavan sir's basic introduction to segments related to business, but the most beneficial aspect was that they were introduced to a completely new subject that too by someone with more than 12years of corporate experience, and 12 years of teaching experience in the field.
         `,
         "coverImage": "1 2.png",
-        "shortDesc":"A workshop students had the opportunity to create a business canvas model of their own Idea.",
-        "tag":"2019-20"
+        "shortDesc": "A workshop students had the opportunity to create a business canvas model of their own Idea.",
+        "tag": "2019-20"
     },
     {
         "id": "financial-advisory",
         "title": "Financial Advisory",
         "date": "3rd March 2020",
-        "conductedBy":"Alpa Shah, TEDxGateway speaker",
+        "conductedBy": "Alpa Shah, TEDxGateway speaker",
         "desc": `
         DJSCE E-CELL had conducted a seminar regarding financial planning for the students. The seminar was conducted by <b>Alpa Shah</b> who is a renowned social speaker and financial professional. With over 25 years of experience, in not only TATA and SBI but also having her own company - <b>Empower Education</b>, Alpa Madam is a torch bearer for the modern women of today's date, and why they should definitely invest. The seminar was to enlighten students regarding financial literacy and money management. Some of the key points discussed in the seminar were:
         <br>
@@ -96,8 +110,8 @@ events_list = [
         ● Power of compounding
         `,
         "coverImage": "d331e43b-6585-47d2-b65e-c71c902bcc19.jpg",
-        "shortDesc":"A seminar regarding financial planning for the students.",
-        "tag":"2019-20"
+        "shortDesc": "A seminar regarding financial planning for the students.",
+        "tag": "2019-20"
     },
     {
         "id": "introduction-event",
@@ -111,7 +125,7 @@ events_list = [
         `,
         "coverImage": "logo.png",
         "shortDesc": "Gyan Dhan had organized sessions for higher education aspirants in DJSCE.",
-        "tag":"2018-19",
+        "tag": "2018-19",
     },
     {
         "id": "mentorship-event",
@@ -136,13 +150,13 @@ events_list = [
         `,
         "coverImage": "event_img.png",
         "shortDesc": "Mentorship Event for the students conducted by our college Alumni Shantanu Baviskar",
-        "tag":"2018-19",
+        "tag": "2018-19",
     },
     {
         "id": "panel-discussion",
         "title": "Panel Discussion",
         "date": "9th March, 2019",
-        "conductedBy":"DJSCE E-CELL committee",
+        "conductedBy": "DJSCE E-CELL committee",
         "desc": `
         On 09th March,2019 Saturday Panel Discussion Event for the students was organised by the ECELL DJSCE committee.
         <br />
@@ -161,23 +175,23 @@ events_list = [
         <br />
         Siddharth Singh (TE IT)
         `,
-        "coverImage":"event_img.png",
-        "shortDesc":"Studnets learnt about funding ventures, the risks of sharing your ideas and finally how can we scale our business.",
-        "tag":"2018-19"
+        "coverImage": "event_img.png",
+        "shortDesc": "Studnets learnt about funding ventures, the risks of sharing your ideas and finally how can we scale our business.",
+        "tag": "2018-19"
     },
     {
         "id": "redbull-event",
         "title": "Redbull Event",
         "date": "2018-19",
-        "conductedBy":"Red Bull",
+        "conductedBy": "Red Bull",
         "desc": `
         Hacking for a Brighter Campus
         <br />
         Red Bull Basement connects the next generation of innovators to turn forward thinking solutions into reality.
         `,
-        "coverImage":"event_img.png",
-        "shortDesc":"Hacking for a Brighter Campus",
-        "tag":"2018-19"
+        "coverImage": "event_img.png",
+        "shortDesc": "Hacking for a Brighter Campus",
+        "tag": "2018-19"
     },
 ]
 
