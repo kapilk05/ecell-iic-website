48c91af324b9455f9eb26296c396d6ce9bdfdcaf
UdayMahajan1 <mahajanuday144@gmail.com>
2023-02-26 21:17:10 +0530

    complete join a team section and display team mates after verification

--- regiSchema.js
+++ regiSchema.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const { default: isEmail } = require('validator/lib/isEmail')
 
-const schema = new mongoose.Schema(
+// schema of the main database : database that lists all the teams
+
+const dbSchema = new mongoose.Schema(
     {
         tName: {
             type: String,
@@ -11,29 +14,43 @@ const schema = new mongoose.Schema(
             //     return validator.notEmpty(value)
             // }
         },
-        eMail: {
+        tLead: {
             type: String,
             required: true,
-            unique: true
-        },
-        pwd: {
-            type: String,
-            required: true
-        },
-        cfPwd: {
+        }
+    }
+)
+
+const Database = mongoose.model('Database', dbSchema)
+
+// schema of a team : attributes of an individual team
+
+const teamSchema = new mongoose.Schema(
+    {
+        pName: {
             type: String,
             required: true
         },
         mobNo: {
             type: Number,
+        },
+        eMail: {
+            type: String,
+            unique: true
+        },
+        tName: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Database',
             required: true
         }
     }
 )
 
-const User = mongoose.model('User', schema)
+const Team = mongoose.model('Team', teamSchema)
 
 module.exports = {
-    schema: schema,
-    User: User
+    dbSchema: dbSchema,
+    Database: Database,
+    teamSchema: teamSchema,
+    Team: Team
 }
